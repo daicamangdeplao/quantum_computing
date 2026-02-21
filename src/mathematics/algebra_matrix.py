b65a5769d01f2b7889bbd557c4354984144376dd
Trung Dung Nguyen <trungdung.nguyen.000000@gmail.com>
2026-02-21 12:59:40 +0100

    review matrix operations

---
+++ src/mathematics/algebra_matrix.py
@@ -0,0 +1,18 @@
+import numpy as np
+#==============================================================================
+# MATRIX
+# - Multiply
+# - Eigenvector/Eigenvalue
+#==============================================================================
+
+mat_a = np.matrix([[1, 2], [3, 4], [5, 6]])
+mat_b = np.matrix([[1], [1]])
+
+mat_ret = np.matmul(mat_a, mat_b)
+
+print(f"Shape of matrix: {mat_a.shape}")
+print(f"Result is {mat_ret} with shape: {mat_ret.shape}")
+
+a = np.matrix([[2, 1], [1, 2]])
+eigenvalues, eigenvectors = np.linalg.eig(a)
+print(f"eigenvalues: {eigenvalues}\neigenvectors: {eigenvectors}")
